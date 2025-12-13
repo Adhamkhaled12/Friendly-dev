2368bda23136c1f3209067d6e48c6e9559d09034
Adham <adhambackshish12@gmail.com>
2025-12-13 02:26:41 +0200

    Add routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,11 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  // index for the home page 
+  index("routes/home/index.tsx"),
+  // to specify a new route for the about page
+  route('about', './routes/about/index.tsx'),
+  route('contact', './routes/contact/index.tsx'),
+  route('projects', './routes/projects/index.tsx'),
+  route('blog', './routes/blog/index.tsx'),
+] satisfies RouteConfig;
